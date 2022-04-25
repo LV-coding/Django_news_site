8eb6ffd0ec705f36f7bd4c19815b78763160e198
LV-coding <96374749+LV-coding@users.noreply.github.com>
2022-04-25 16:20:54 +0300

    edit debug

--- news_aggregator/settings.py
+++ news_aggregator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', "some_secret_key")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
